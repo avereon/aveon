20381595a0d4c01d43829431ada3f5c51a42386e
Mark Soderquist <mark@soderquist.net>
2020-03-18 13:41:33 -0600

    Work on loading airfoil data

--- source/main/java/com/avereon/aveon/AirfoilCodec.java
+++ source/main/java/com/avereon/aveon/AirfoilCodec.java
@@ -1,5 +1,6 @@
 package com.avereon.aveon;
 
+import com.avereon.util.Log;
 import com.avereon.util.TextUtil;
 import com.avereon.xenon.asset.Asset;
 import com.avereon.xenon.asset.Codec;
@@ -8,9 +9,12 @@ import javafx.geometry.Point2D;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AirfoilCodec extends Codec {
+
+	private static final System.Logger log = Log.get();
 
 	@Override
 	public String getKey() {
@@ -44,39 +48,29 @@ public class AirfoilCodec extends Codec {
 		}
 
 		// If line 3 is blank then it is Lednicer format otherwise it is Selig format
-		if( TextUtil.isEmpty( result.get( 2 ) ) ) {
-			loadLednicer( asset, result );
-		} else {
-			loadSelig( asset, result );
-		}
+		boolean lednicer = TextUtil.isEmpty( result.get( 2 ) );
+		Airfoil foil = lednicer ? loadLednicer( result ) : loadSelig( result );
+
+		asset.setModel( foil );
 	}
 
 	@Override
 	public void save( Asset asset, OutputStream output ) throws IOException {}
 
-	Airfoil loadLednicer( Asset asset, List<String> lines ) {
+	Airfoil loadLednicer( List<String> lines ) {
+		int index = 0;
+
 		// First line is name
-		String name = lines.get( 0 ).trim();
+		String name = lines.get( index++ ).trim();
 
 		// Second line is upper point count and lower point count
-		// blank line
-
-		// nose to tail upper coords x,y
-		String line;
-		int index = 0;
-		List<Point2D> upper = new ArrayList<>();
-		while( !TextUtil.isEmpty( line = lines.get( index++ ) ) ) {
-			Point2D point = parsePoint( line );
-		}
-
-		// blank line
 		index++;
 
-		// nose to tail lower coords x,y
+		List<Point2D> upper = new ArrayList<>();
+		index = loadPoints( index, lines, upper );
+
 		List<Point2D> lower = new ArrayList<>();
-		while( !TextUtil.isEmpty( line = lines.get( index++ ) ) ) {
-			Point2D point = parsePoint( line );
-		}
+		index = loadPoints( index, lines, lower );
 
 		Airfoil airfoil = new Airfoil();
 		airfoil.setName( name );
@@ -85,18 +79,56 @@ public class AirfoilCodec extends Codec {
 		return airfoil;
 	}
 
-	Airfoil loadSelig( Asset asset, List<String> lines ) {
+	Airfoil loadSelig( List<String> lines ) {
+		int index = 0;
+
 		// First line is name
+		String name = lines.get( index++ ).trim();
+
 		// tail to nose upper coords x,y
-		// there should be one point at 0,0 (but not exactly at 0,0)
+		List<Point2D> upper = new ArrayList<>();
+		index = loadPoints( index, lines, upper, true );
+		Collections.reverse( upper );
+
 		// nose to tail lower coords x,y
+		List<Point2D> lower = new ArrayList<>();
+		index = loadPoints( index, lines, lower );
 
 		Airfoil airfoil = new Airfoil();
+		airfoil.setName( name );
+		airfoil.setUpper( upper );
+		airfoil.setLower( lower );
 		return airfoil;
 	}
 
-	Point2D parsePoint( String line ) {
-		String[] values = line.split( " " );
+	private int loadPoints( int index, List<String> lines, List<Point2D> upper ) {
+		return loadPoints( index, lines, upper, false );
+	}
+
+	private int loadPoints( int index, List<String> lines, List<Point2D> upper, boolean stopOnTurn ) {
+		// blank lines
+		while( index < lines.size() && TextUtil.isEmpty( lines.get( index ) ) ) {
+			index++;
+		}
+
+		// points
+		String line;
+		double x = 1;
+		while( index < lines.size() && !TextUtil.isEmpty( line = lines.get( index ) ) ) {
+			Point2D point = loadPoint( line );
+			if( stopOnTurn && point.getX() > x ) {
+				index--;
+				break;
+			}
+			upper.add( point );
+			x = point.getX();
+			index++;
+		}
+		return index;
+	}
+
+	Point2D loadPoint( String line ) {
+		String[] values = line.trim().split( "\\s+" );
 		return new Point2D( Double.parseDouble( values[ 0 ] ), Double.parseDouble( values[ 1 ] ) );
 	}
 
--- source/test/java/com/avereon/aveon/AirfoilCodedTest.java
+++ source/test/java/com/avereon/aveon/AirfoilCodedTest.java
@@ -4,10 +4,21 @@ import javafx.geometry.Point2D;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class AirfoilCodedTest {
+
+	private static final String HT05_IL_LED = "http://airfoiltools.com/airfoil/lednicerdatfile?airfoil=ht05-il";
+
+	private static final String HT05_IL_SEL = "http://airfoiltools.com/airfoil/seligdatfile?airfoil=ht05-il";
+
+	private static final String CLARKY_IL_LED = "http://airfoiltools.com/airfoil/lednicerdatfile?airfoil=clarky-il";
+
+	private static final String CLARKY_IL_SEL = "http://airfoiltools.com/airfoil/seligdatfile?airfoil=clarky-il";
 
 	private AirfoilCodec codec;
 
@@ -17,16 +28,65 @@ public class AirfoilCodedTest {
 	}
 
 	@Test
-	void testLoadSelig() {
-		String content = "";
+	void testLoadLednicer() throws Exception {
+		List<String> lines = new ArrayList<>();
 
-		//codec.loadSelig( asset, lines);
+		lines.add( "CLARK Y AIRFOIL" );
+		lines.add( "       61.       61." );
+		lines.add( "" );
+		lines.add( "  0.000000  0.000000" );
+		lines.add( "  0.360000  0.091627" );
+		lines.add( "  1.000000  0.000599" );
+		lines.add( "" );
+		lines.add( "  0.000000  0.000000" );
+		lines.add( "  0.160000 -0.030255" );
+		lines.add( "  1.000000 -0.000599" );
+
+		Airfoil foil = codec.loadLednicer( lines );
+
+		assertThat( foil.getName(), is( "CLARK Y AIRFOIL" ) );
+		int index = 0;
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 0, 0 ) ) );
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 0.36, 0.091627 ) ) );
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 1, 0.000599 ) ) );
+		assertThat( index, is( 3 ) );
+		index = 0;
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 0, 0 ) ) );
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 0.160000, -0.030255 ) ) );
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 1, -0.000599 ) ) );
+		assertThat( index, is( 3 ) );
+	}
+
+	@Test
+	void testLoadSelig() throws Exception {
+		List<String> lines = new ArrayList<>();
+
+		lines.add( "CLARK Y AIRFOIL" );
+		lines.add( "  1.000000  0.000599" );
+		lines.add( "  0.360000  0.091627" );
+		lines.add( "  0.000000  0.000000" );
+		lines.add( "  0.160000 -0.030255" );
+		lines.add( "  1.000000 -0.000599" );
+
+		Airfoil foil = codec.loadSelig( lines );
+
+		assertThat( foil.getName(), is( "CLARK Y AIRFOIL" ) );
+		int index = 0;
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 0, 0 ) ) );
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 0.36, 0.091627 ) ) );
+		assertThat( foil.getUpper().get( index++ ), is( new Point2D( 1, 0.000599 ) ) );
+		assertThat( index, is( 3 ) );
+		index = 0;
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 0, 0 ) ) );
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 0.160000, -0.030255 ) ) );
+		assertThat( foil.getLower().get( index++ ), is( new Point2D( 1, -0.000599 ) ) );
+		assertThat( index, is( 3 ) );
 	}
 
 	@Test
 	void testParsePoint() {
-		assertThat( codec.parsePoint( "0 0" ), is( Point2D.ZERO ) );
-		assertThat( codec.parsePoint( "0.25 0.01" ), is( new Point2D( 0.25, 0.01 ) ) );
+		assertThat( codec.loadPoint( "0 0" ), is( Point2D.ZERO ) );
+		assertThat( codec.loadPoint( "  0.25  0.01" ), is( new Point2D( 0.25, 0.01 ) ) );
 	}
 
 }

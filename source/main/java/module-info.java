1d0675dbcfbf8eb6cc24af0e4884bc53e772cc02
Mark Soderquist <mark@soderquist.net>
2021-10-09 19:20:29 -0600

    Version 1.0

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -7,14 +7,14 @@ module com.avereon.aveon {
 	requires com.avereon.curve;
 	requires com.avereon.xenon;
 	requires com.avereon.zenna;
-	requires com.avereon.zerra;
+	requires com.avereon.zarra;
 	requires com.avereon.zevra;
 	requires javafx.controls;
 	requires javafx.graphics;
 
 	opens com.avereon.aveon.bundles;
 
-	exports com.avereon.aveon to com.avereon.xenon, com.avereon.zerra;
+	exports com.avereon.aveon to com.avereon.xenon, com.avereon.zarra;
 	exports com.avereon.geometry;
 
 	provides com.avereon.xenon.Mod with com.avereon.aveon.Aveon;

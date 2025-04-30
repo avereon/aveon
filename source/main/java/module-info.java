489776b977e30560a018cd463abee7fd1a050792
msoderquist <msoderquist@mastercontrol.com>
2025-04-29 21:19:24 -0600

    Fix import statements to use 'zerra' instead of 'zarra'

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -10,14 +10,14 @@ module com.avereon.aveon {
 	requires com.avereon.marea;
 	requires com.avereon.xenon;
 	requires com.avereon.zenna;
-	requires com.avereon.zarra;
+	requires com.avereon.zerra;
 	requires com.avereon.zevra;
 	requires javafx.controls;
 	requires javafx.graphics;
 
 	opens com.avereon.aveon.bundles;
 
-	exports com.avereon.aveon to com.avereon.xenon, com.avereon.zarra;
+	exports com.avereon.aveon to com.avereon.xenon, com.avereon.zerra;
 	exports com.avereon.geometry;
 
 	provides Module with com.avereon.aveon.Aveon;

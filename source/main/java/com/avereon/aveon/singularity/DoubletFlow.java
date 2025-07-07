c71cb9a738c8a280f2133d193d5ff341e0919b99
Mark Soderquist <mark@soderquist.net>
2025-07-07 17:30:13 -0600

    Add `DoubletFlow` implementation and parameterized tests
    Implement the `DoubletFlow` class extending `ElementaryFlow`, providing methods for velocity calculation. Add parameterized tests to validate functionality with comprehensive test cases.

---
+++ source/main/java/com/avereon/aveon/singularity/DoubletFlow.java
@@ -0,0 +1,49 @@
+package com.avereon.aveon.singularity;
+
+import com.avereon.aveon.ElementaryFlow;
+
+public class DoubletFlow implements ElementaryFlow {
+
+	private final double x;
+
+	private final double y;
+
+	private final double a;
+
+	private final double q;
+
+	public DoubletFlow( double x, double y, double a, double q ) {
+		this.x = x;
+		this.y = y;
+		this.a = a;
+		this.q = q;
+	}
+
+	@Override
+	public double getStream( double x, double y ) {
+		return 0;
+	}
+
+	@Override
+	public double getPotential( double x, double y ) {
+		return 0;
+	}
+
+	@Override
+	public double[] getVelocity( double xf, double yf ) {
+		double r2, xr, yr;
+		double[] velocity = new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY };
+
+		xr = xf - x;
+		yr = yf - y;
+		r2 = xr * xr + yr * yr;
+
+		if( r2 > 0.0 ) {
+			velocity[ 0 ] = -q / r2 / r2 * ((xr * xr - yr * yr) * Math.cos( a ) + 2 * xr * yr * Math.sin( a ));
+			velocity[ 1 ] = q / r2 / r2 * (-(xr * xr - yr * yr) * Math.sin( a ) + 2 * xr * yr * Math.cos( a ));
+		}
+
+		return velocity;
+	}
+
+}
---
+++ source/test/java/com/avereon/aveon/singularity/DoubletFlowTest.java
@@ -0,0 +1,66 @@
+package com.avereon.aveon.singularity;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DoubletFlowTest {
+
+	@ParameterizedTest
+	@MethodSource
+	void velocity( double x, double y, double a, double q, double fx, double fy, double[] expected ) {
+		// given
+		DoubletFlow flow = new DoubletFlow( x, y, a, q );
+
+		// then
+		assertThat( flow.getVelocity( fx, fy ) ).isEqualTo( expected );
+	}
+
+	static Stream<Arguments> velocity() {
+		List<Arguments> arguments = new ArrayList<>();
+
+		double x = 0;
+		double y = 0;
+		double a = 0;
+		double q = 1;
+
+		arguments.add( Arguments.of( x, y, a, q, -2, -2, new double[]{ -0.0, 0.125 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -2, -1, new double[]{ -0.12, 0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -2, 0, new double[]{ -0.25, -0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -2, 1, new double[]{ -0.12, -0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -2, 2, new double[]{ -0.0, -0.125 } ) );
+
+		arguments.add( Arguments.of( x, y, a, q, -1, -2, new double[]{ 0.12, 0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -1, -1, new double[]{ -0.0, 0.5 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -1, 0, new double[]{ -1.0, -0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -1, 1, new double[]{ -0.0, -0.5 } ) );
+		arguments.add( Arguments.of( x, y, a, q, -1, 2, new double[]{ 0.12, -0.16 } ) );
+
+		arguments.add( Arguments.of( x, y, a, q, 0, -2, new double[]{ 0.25, 0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 0, -1, new double[]{ 1.0, 0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 0, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
+		arguments.add( Arguments.of( x, y, a, q, 0, 1, new double[]{ 1.0, 0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 0, 2, new double[]{ 0.25, 0.0 } ) );
+
+		arguments.add( Arguments.of( x, y, a, q, 1, -2, new double[]{ 0.12, -0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 1, -1, new double[]{ -0.0, -0.5 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 1, 0, new double[]{ -1.0, 0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 1, 1, new double[]{ -0.0, 0.5 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 1, 2, new double[]{ 0.12, 0.16 } ) );
+
+		arguments.add( Arguments.of( x, y, a, q, 2, -2, new double[]{ -0.0, -0.125 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 2, -1, new double[]{ -0.12, -0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 2, 0, new double[]{ -0.25, 0.0 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 2, 1, new double[]{ -0.12, 0.16 } ) );
+		arguments.add( Arguments.of( x, y, a, q, 2, 2, new double[]{ -0.0, 0.125 } ) );
+
+		return arguments.stream();
+	}
+
+}

218be39194321f2d51874773322ee54729ea7548
Mark Soderquist <mark@soderquist.net>
2025-07-07 17:01:42 -0600

    Add `SourceFlow` implementation and parameterized tests
    Implement the `SourceFlow` class extending `ElementaryFlow`, providing methods for velocity calculation. Add comprehensive parameterized tests to validate behavior.

---
+++ source/main/java/com/avereon/aveon/singularity/SourceFlow.java
@@ -0,0 +1,45 @@
+package com.avereon.aveon.singularity;
+
+import com.avereon.aveon.ElementaryFlow;
+
+public class SourceFlow implements ElementaryFlow {
+
+	private final double x;
+
+	private final double y;
+
+	private final double q;
+
+	public SourceFlow( double x, double y, double q ) {
+		this.x = x;
+		this.y = y;
+		this.q = q;
+	}
+
+	@Override
+	public double getStream( double x, double y ) {
+		return 0;
+	}
+
+	@Override
+	public double getPotential( double x, double y ) {
+		return 0;
+	}
+
+	@Override
+	public double[] getVelocity( double xf, double yf ) {
+		double r2, xr, yr;
+		double[] velocity = new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY };
+
+		xr = xf - x;
+		yr = yf - y;
+		r2 = xr * xr + yr * yr;
+
+		if( r2 > 0.0 ) {
+			velocity[ 0 ] = q / r2 * xr;
+			velocity[ 1 ] = q / r2 * yr;
+		}
+		return velocity;
+	}
+
+}

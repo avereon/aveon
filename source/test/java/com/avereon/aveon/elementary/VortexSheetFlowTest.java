bfca5cb5c47cbecc971464bfcea5e5f4928f21a9
Mark Soderquist <mark@soderquist.net>
2025-07-07 20:08:26 -0600

    Update `SourceSheetFlowTest` and `VortexSheetFlowTest` parameters for consistency
    Adjust test method signatures and test data in `SourceSheetFlowTest` and `VortexSheetFlowTest` to include additional parameters for potential and streamline calculations. Ensure consistency across parameterized tests.

--- source/test/java/com/avereon/aveon/elementary/VortexSheetFlowTest.java
+++ source/test/java/com/avereon/aveon/elementary/VortexSheetFlowTest.java
@@ -14,7 +14,7 @@ public class VortexSheetFlowTest {
 
 	@ParameterizedTest
 	@MethodSource
-	void velocity( double x1, double y1, double x2, double y2, double q, double fx, double fy, double[] expected ) {
+	void velocity( double x1, double y1, double x2, double y2, double q, double fx, double fy, double p, double s, double[] expected ) {
 		// given
 		VortexSheetFlow flow = new VortexSheetFlow( x1, y1, x2, y2, q );
 
@@ -32,35 +32,35 @@ public class VortexSheetFlowTest {
 		double q = 1;
 
 		// This is the right idea, but most of the test values are incorrect
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, -2, new double[]{ 0.25, -0.25 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, -1, new double[]{ 0.2, -0.4 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 0, new double[]{ -0.0, -0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 1, new double[]{ -0.2, -0.4 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 2, new double[]{ -0.25, -0.25 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, -2,0,0,new double[]{ 0.25, -0.25 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, -1,0,0,new double[]{ 0.2, -0.4 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 0,0,0,new double[]{ -0.0, -0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 1,0,0,new double[]{ -0.2, -0.4 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -2, 2,0,0,new double[]{ -0.25, -0.25 } ) );
 		//
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, -2, new double[]{ 0.4, -0.2 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, -1, new double[]{ 0.5, -0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 0, new double[]{ -0.0, -1.0 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 1, new double[]{ -0.5, -0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 2, new double[]{ -0.4, -0.2 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, -2,0,0,new double[]{ 0.4, -0.2 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, -1,0,0,new double[]{ 0.5, -0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 0,0,0,new double[]{ -0.0, -1.0 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 1,0,0,new double[]{ -0.5, -0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, -1, 2,0,0,new double[]{ -0.4, -0.2 } ) );
 		//
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, -2, new double[]{ 0.5, 0.0 } ) );
-		arguments.add( Arguments.of( x1, y1, x2, y2, q, -1, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
-		arguments.add( Arguments.of( x1, y1, x2, y2, q, 1, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, 2, new double[]{ -0.5, 0.0 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, -2,0,0,new double[]{ 0.5, 0.0 } ) );
+		arguments.add( Arguments.of( x1, y1, x2, y2, q, -1, 0, 0, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, 0,0,0,new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
+		arguments.add( Arguments.of( x1, y1, x2, y2, q, 1, 0, 0, 0, new double[]{ Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 0, 2,0,0,new double[]{ -0.5, 0.0 } ) );
 		//
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, -2, new double[]{ 0.4, 0.2 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, -1, new double[]{ 0.5, 0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 0, new double[]{ -0.0, 1.0 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 1, new double[]{ -0.5, 0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 2, new double[]{ -0.4, 0.2 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, -2,0,0,new double[]{ 0.4, 0.2 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, -1,0,0,new double[]{ 0.5, 0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 0,0,0,new double[]{ -0.0, 1.0 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 1,0,0,new double[]{ -0.5, 0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 1, 2,0,0,new double[]{ -0.4, 0.2 } ) );
 		//
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, -2, new double[]{ 0.25, 0.25 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, -1, new double[]{ 0.2, 0.4 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 0, new double[]{ -0.0, 0.5 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 1, new double[]{ -0.2, 0.4 } ) );
-		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 2, new double[]{ -0.25, 0.25 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, -2,0,0,new double[]{ 0.25, 0.25 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, -1,0,0,new double[]{ 0.2, 0.4 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 0,0,0,new double[]{ -0.0, 0.5 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 1,0,0,new double[]{ -0.2, 0.4 } ) );
+		//		arguments.add( Arguments.of( x1,y1,x2,y2, q, 2, 2,0,0,new double[]{ -0.25, 0.25 } ) );
 
 		return arguments.stream();
 	}
